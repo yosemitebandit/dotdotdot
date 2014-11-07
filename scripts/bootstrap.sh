38ab3ee3c91ea1bc1ad9f8c869752ad4874e22f7
Matt Ball <matt.ball.2@gmail.com>
2014-11-07 05:38:42 +0000

    adds xdg-utils to bootstrap script

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -24,7 +24,8 @@ sudo apt-get -y install git-core git-gui gitk zsh vim-gtk exuberant-ctags \
                         python-software-properties python-pip python-dev \
                         python-virtualenv build-essential gfortran \
                         libopenblas-dev liblapack-dev libfreetype6-dev \
-                        libpng-dev openjdk-7-jdk tmux silversearcher-ag tree
+                        libpng-dev openjdk-7-jdk tmux silversearcher-ag tree \
+                        xdg-utils
 # add-apt-repo may not be available until after first upgrade
 sudo add-apt-repository ppa:nmi/vim-snapshots -y
 sudo apt-get update -y
